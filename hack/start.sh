17f75b732cf156350010708d4ba50497ae7de6a4
nfyxhan <nfyxhan@163.com>
2023-08-14 15:01:39 +0800

    docker option

--- hack/start.sh
+++ hack/start.sh
@@ -5,9 +5,9 @@ addr=${addr:-0.0.0.0}
 data_path=${data_path:-/data}
 
 echo docker run -itd --security-opt=seccomp:unconfined \
+    ${DOCKER_OPTION} \
     --name workspace \
     -p ${port}:${port} \
-    -p 80:80 \
     -v ${data_path}:${data_path} \
     "${FULL_IMAGE}" \
-    code-server --bind-addr ${addr}:${port} /data
+    code-server --bind-addr ${addr}:${port} ${data_path}

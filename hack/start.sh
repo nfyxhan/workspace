c3137b8e23010d57d9771523ff4f308c0f73ce0a
nfyxhan <nfyxhan@163.com>
2023-10-07 17:59:05 +0800

    bash

--- hack/start.sh
+++ hack/start.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-port=${port:-9999}
-addr=${addr:-0.0.0.0}
 data_path=${data_path:-/data}
 
 echo docker run -itd --security-opt=seccomp:unconfined \
     ${DOCKER_OPTION} \
     --name workspace \
-    -p ${port}:${port} \
     -v ${data_path}:${data_path} \
     "${FULL_IMAGE}" \
-    /bin/bash -c '"set -e && code-server --bind-addr ${addr}:${port} '${data_path}'"'
+    /bin/bash

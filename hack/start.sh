aa9b92e699e502be7d01e6bcab93f6522746f43d
nfyxhan <15644272+nfyxhan@users.noreply.github.com>
2024-01-26 16:06:55 +0800

    containerName

--- hack/start.sh
+++ hack/start.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 data_path=${data_path:-/data}
+container=${container:-workspace}
 
 echo docker run -itd --security-opt=seccomp:unconfined \
     ${DOCKER_OPTION} \
-    --name workspace \
+    --name ${container} \
     -v ${data_path}:${data_path} \
     "${FULL_IMAGE}" bash

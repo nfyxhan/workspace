5cf1f729ae27e747b89f672fe9f92cd7255da28e
nfyxhan <nfyxhan@163.com>
2024-03-18 17:16:16 +0800

    lscpu | grep Architecture

--- hack/env.sh
+++ hack/env.sh
@@ -2,8 +2,17 @@
 
 echo "TargetPlatform is ${TARGETPLATFORM}"
 
+lscpu | grep Architecture
+
+arch=`lscpu | grep Architecture | awk '{print $2}'`
+
+
 if [[ "${TARGETPLATFORM}" == "linux/arm64" ]] ; then 
     export RUN_PLATFORM=arm64
 else
     export RUN_PLATFORM=amd64
+fi
+
+if [[ "${arch}" != "x86_64" ]] ; then
+    export RUN_PLATFORM=arm64
 fi
